614bd97fd6971ef5cbaa663ae0b4a65c2503f700
rishav <betusenger@gmail.com>
2022-07-24 05:07:19 +0530

    base home rendering

--- base/views.py
+++ base/views.py
@@ -1,11 +1,25 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpRequest
+from matplotlib.style import context
 
 # Create your views here.
 
-def home(request):
-    return render(request, 'home.html')
 
-def room(request):
-    return render(request,'room.html') 
+rooms = [
+    {'id':1, 'name': 'learning python'},
+    {'id':2, 'name': 'learning javascript'},
+    {'id':3, 'name': 'learning golang'},
+]
+
+def home(request):
+    context = {'rooms':rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request,pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request,'base/room.html', context) 
