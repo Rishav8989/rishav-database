3bebc70fa326adabf824c00625aabd0853b297c6
rishav <betusenger@gmail.com>
2022-07-24 03:08:08 +0530

    base home rendering

--- base/views.py
+++ base/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home(request):
+    return HttpResponse('home')
+
+def room(Request):
+    return HttpResponse('room')
