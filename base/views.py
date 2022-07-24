9646a765b23a74ea9eec51e624055e5feff73bec
rishav <betusenger@gmail.com>
2022-07-25 03:29:01 +0530

    Forms

--- base/views.py
+++ base/views.py
@@ -2,24 +2,29 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpRequest
 from matplotlib.style import context
+from . models import Room
+from .forms import RoomForm
 
 # Create your views here.
 
 
-rooms = [
+"""rooms = [
     {'id':1, 'name': 'learning python'},
     {'id':2, 'name': 'learning javascript'},
     {'id':3, 'name': 'learning golang'},
-]
+]"""
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms':rooms}
     return render(request, 'base/home.html', context)
 
 def room(request,pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request,'base/room.html', context) 
+
+def createRoom(request):
+    form = RoomForm()
+    context = {'form':form}
+    return render(request, 'base/room_form.html', context)
